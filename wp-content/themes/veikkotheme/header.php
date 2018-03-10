5665fabd868ec5f0e8fb05cc6aba061ca714a2cd
veikkokiisiktkhk <veikko.kiis@khk.ee>
2018-03-10 16:10:32 +0200

    installed theme `Veikko Theme` version 1.0

---
+++ wp-content/themes/veikkotheme/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>bootstrap website using photoshop</title>
+
+
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+<?php wp_head(); ?>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 header_kast"><h1 id="header_text">See on minu header!</h1></div>		
+		</div>
---
+++ wp-content/themes/veikkotheme/index.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+
+
+
+		<div class="row">
+			<div class="col-lg-6 vasak_kast"><p id="header_text">info1</p></div>
+			<div class="col-lg-6 parem_kast"><p id="header_text">info2</p></div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12 vahe1"></div>
+		</div>
+		<div class="row">
+			<div class="col-lg-3 vaike_kast"></div>
+			<div class="col-lg-4"></div>
+			<div class="col-lg-4"><h1 class="mytext">My header</h1></div>
+
+		</div>
+		<div class="row">
+			<div class="col-lg-12 vahe2"></div>
+		</div>
+
+<?php get_footer(); ?>
